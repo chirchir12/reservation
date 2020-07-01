fe6d0ad1400cacadb6f51957f5827f4a76b3af31
chirchir <chirchir7370@gmail.com>
2020-07-01 15:25:56 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import SingleRoom from './Pages/SingleRoom';
@@ -8,10 +9,9 @@ function App() {
   return (
     <div className="Appp">
       <h1>resort hotel</h1>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms" component={Rooms} />
+      <Route exact path="/single-room" component={SingleRoom} />
     </div>
   );
 }
