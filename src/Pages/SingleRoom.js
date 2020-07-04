9619fb994d37f3314499746d1ad3aa345f195c94
chirchir <chirchir7370@gmail.com>
2020-07-04 16:14:52 +0300

    finish singleroom component

--- src/Pages/SingleRoom.js
+++ src/Pages/SingleRoom.js
@@ -48,6 +48,38 @@ class SingleRoo extends Component {
             </Link>
           </Banner>
         </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {images.map((image, index) => (
+              <img key={index} src={image} alt={name} />
+            ))}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>Details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>Info</h3>
+              <h6>Price : ${price}</h6>
+              <h6>Size : {size} SQFT</h6>
+              <h6>
+                max capacity:{' '}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6> {pets ? 'Pets allowed' : 'no pets allowed'} </h6>
+              <h6>{breakfast && 'free breakfast'}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, idx) => {
+              return <li key={idx}>- {item}</li>;
+            })}
+          </ul>
+        </section>
       </>
     );
   }
